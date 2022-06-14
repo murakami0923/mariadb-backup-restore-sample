12ae3e3dad8f2286200f2ad9619e3670ee141e56
murakami <murakami@bitstar.jp>
2022-06-14 12:25:02 +0000

    DB更新（レコード追加）を5秒間隔で実行するように変更。

--- dockerfile/app/update-db.sh
+++ dockerfile/app/update-db.sh
@@ -5,12 +5,18 @@ DB_HOST={{DB_HOST}}
 DB_USER={{DB_USER}}
 DB_NAME={{DB_NAME}}
 
-shuf=`shuf -i 1-100000000 -n 1`; name_sum=`echo -n ${shuf} | sha256sum `
-created_at=`date '+%Y-%m-%d %H:%M:%S'`
+interval=5
 
-sql="INSERT INTO test_data (name, created_at) VALUES ('${name_sum}', '${created_at}');"
-echo ${sql} >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
+for i in `seq 0 ${interval} 59`; do
+    if [ ${i} -ne 0 ]; then
+        sleep ${interval};
+    fi
 
-mysql -h ${DB_HOST} -u ${DB_USER} ${DB_NAME} -e "${sql}" >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
+    shuf=`shuf -i 1-100000000 -n 1`; name_sum=`echo -n ${shuf} | sha256sum `
+    created_at=`date '+%Y-%m-%d %H:%M:%S'`
 
-# mysql -h ${DB_HOST} -u ${DB_USER} ${DB_NAME} < /root/update-db.sql >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
+    sql="INSERT INTO test_data (name, created_at) VALUES ('${name_sum}', '${created_at}');"
+    echo ${sql} >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
+
+    mysql -h ${DB_HOST} -u ${DB_USER} ${DB_NAME} -e "${sql}" >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
+done
