1ce60207730d2223164b7fff4fee48aa54e4e0bd
murakami <murakami@bitstar.jp>
2022-06-14 02:53:40 +0000

    テスト用テーブルのidを自動連番に設定。

--- dockerfile/app/init-db.sql
+++ dockerfile/app/init-db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `test_data` (
-    `id` INT NOT NULL PRIMARY KEY,
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(255),
     `created_at` DATETIME
 )
