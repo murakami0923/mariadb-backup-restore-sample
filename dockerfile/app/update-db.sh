8e19e9caf964934024e9d2b6b0cb8fb0bcd48a80
murakami <murakami@bitstar.jp>
2022-06-14 04:16:46 +0000

    DB更新処理を、SQLファイル実行から、都度SQL生成・実行に変更。

--- dockerfile/app/update-db.sh
+++ dockerfile/app/update-db.sh
@@ -5,4 +5,12 @@ DB_HOST={{DB_HOST}}
 DB_USER={{DB_USER}}
 DB_NAME={{DB_NAME}}
 
-mysql -h ${DB_HOST} -u ${DB_USER} ${DB_NAME} < /root/update-db.sql >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
+shuf=`shuf -i 1-100000000 -n 1`; name_sum=`echo -n ${shuf} | sha256sum `
+created_at=`date '+%Y-%m-%d %H:%M:%S'`
+
+sql="INSERT INTO test_data (name, created_at) VALUES ('${name_sum}', '${created_at}');"
+echo ${sql} >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
+
+mysql -h ${DB_HOST} -u ${DB_USER} ${DB_NAME} -e "${sql}" >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
+
+# mysql -h ${DB_HOST} -u ${DB_USER} ${DB_NAME} < /root/update-db.sql >> /root/logs/update-db-`date '+%Y%m%d'`.log 2>&1
